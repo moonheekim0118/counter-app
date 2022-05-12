3abeb1e3f48db67958af410204ad9ebf031d85a6
moonheekim0118 <hellomooneekim@gmail.com>
2022-05-12 12:23:36 +0900

    feat: 구현
    Co-authored-by: Marco <wonsss@users.noreply.github.com>

--- src/react-redux/useSelector.jsx
+++ src/react-redux/useSelector.jsx
@@ -4,14 +4,8 @@ const useSelector = (selector) => {
   const store = useStore();
   const [state, setState] = useState(selector(store.getState()));
 
-  console.log(store.getState());
-  console.log(selector);
-
-  console.log(selector(store.getState()));
-
   useEffect(() => {
     const callback = () => {
-      // const state = store.getState();
       const state = selector(store.getState());
 
       setState(state);
